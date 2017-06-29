6b4e8a08cf0a5acb435e92808e6840ac62d698bd
Ankur Srivastava <ankur.srivastava@recogizer.de>
2017-06-29 13:20:00 +0200

    new build

--- commands.go
+++ commands.go
@@ -1,3 +1,25 @@
+// MIT License
+//
+// Copyright (c) 2017 Ankur Srivastava
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package main
 
 import (
@@ -9,8 +31,8 @@ import (
 )
 
 var (
-	GetIgnore = downloader.New()
-	rootCmd    = &cobra.Command{Use: "getignore"}
+	getIgnore = downloader.New()
+	rootCmd   = &cobra.Command{Use: "getignore"}
 )
 
 func init() {
@@ -25,7 +47,7 @@ var cmdList = &cobra.Command{
 	Short: "Display a list of available gitignore files.",
 	Long:  `Display a list of available gitignore files available on github.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		GetIgnore.ListLanguages(true)
+		getIgnore.ListLanguages(true)
 	},
 }
 
@@ -42,6 +64,6 @@ var cmdDwnld = &cobra.Command{
 		}
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-		GetIgnore.DownloadFile(args[0])
+		getIgnore.DownloadFile(args[0])
 	},
 }
